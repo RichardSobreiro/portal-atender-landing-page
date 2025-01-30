80865199ce2dc4102ad4a02c1bd2dc31b72a19b7
Richard Sobreiro <richardsobreiro@gmail.com>
2025-01-30 09:17:01 -0300

    FIT: SEO updates

---
+++ landing-page/next-sitemap.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: 'https://portalatender.com.br',
+  generateRobotsTxt: true,
+  sitemapSize: 5000, // Optional, adjust if you have a large number of pages
+  outDir: './public', // Ensure the sitemap is placed in the public folder
+};
--- landing-page/next-sitemap.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  siteUrl: 'https://portalatender.com.br',
-  generateRobotsTxt: true,
-};
