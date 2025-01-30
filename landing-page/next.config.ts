a990f2515ed8c452572531c9267a6750dc7fc4a0
Richard Sobreiro <richardsobreiro@gmail.com>
2025-01-30 09:33:50 -0300

    FIT: SSG set up

--- landing-page/next.config.ts
+++ landing-page/next.config.ts
@@ -1,8 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  output: 'export',
+  // Export your app as a static site
+  exportPathMap: async function () {
+    return {
+      '/': { page: '/' },
+    };
+  },
+  // You can optionally customize more settings here
+  images: {
+    unoptimized: true, // If you're having issues with image optimization
+  },
 };
 
 export default nextConfig;
